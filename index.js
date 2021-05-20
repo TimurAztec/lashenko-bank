8a5817a4ff8d7beb7cd8193db823230d56c9b667
timotiaztec <timotiaztec@gmail.com>
2021-05-20 13:20:51 +0300

    Made mongo db shit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const PORT = process.env.PORT || 80;
 const http = require('http');
 const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:admin@bank.v5c9f.mongodb.net/bank?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const server = http.createServer((request, response) => {
     response.setHeader('Access-Control-Allow-Origin', '*');
@@ -10,8 +13,59 @@ const server = http.createServer((request, response) => {
             response.write(data);
             response.end();
         });
-    }
-    else {
+    } else if (request.url.split('/')[1] === 'api') {
+        if (request.url.split('/')[2] === 'signup' && request.method === 'POST') {
+            let dataJSONString = '';
+            request.on('data', (data) => {
+                dataJSONString += data;
+            });
+            request.on('end', () => {
+                const data = JSON.parse(dataJSONString);
+                client.connect((err, client) => {
+                    if (err) throw err;
+                    const collection = client.db('bank').collection('users');
+                    collection.findOne({login: data.login}).then((findres) => {
+                        if (findres) {
+                            response.writeHead(404);
+                            response.end(JSON.stringify('Such user already exist'));
+                            client.close();
+                        } else {
+                            collection.insertOne(data).then((insertres) => {
+                                response.writeHead(200);
+                                response.end(JSON.stringify(insertres));
+                            }).finally(() => client.close());
+                        }
+                    });
+                });
+
+            });
+        }
+        if (request.url.split('/')[2] === 'signin' && request.method === 'POST') {
+            let dataJSONString = '';
+            request.on('data', (data) => {
+                dataJSONString += data;
+            });
+            request.on('end', () => {
+                const data = JSON.parse(dataJSONString);
+                client.connect((err, client) => {
+                    if (err) throw err;
+                    const collection = client.db('bank').collection('users');
+                    collection.findOne(data).then((findres) => {
+                        if (findres) {
+                            response.writeHead(200);
+                            response.end(JSON.stringify(findres));
+                            client.close();
+                        } else {
+                            response.writeHead(404);
+                            response.end(JSON.stringify('Wrong sign in data!'));
+                            client.close();
+                        }
+                    }).finally(() => {});
+                });
+
+            });
+        }
+    } else {
         fs.readFile(`${__dirname}/front${request.url}`, function (err,data) {
             if (err) {
                 response.writeHead(404);
